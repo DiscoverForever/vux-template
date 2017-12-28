f30f7ec0f3b4afe4f5d881283da92b7e08d5ecad
tangyy <tangyueyang@lztogether.com>
2017-12-28 18:10:14 +0800

    Add element-ui

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import FastClick from 'fastclick'
+import Vue from 'vue';
+import FastClick from 'fastclick';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 import router from './router';
-import App from './App'
+import App from './App';
 
+Vue.use(ElementUI);
 
-FastClick.attach(document.body)
+FastClick.attach(document.body);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app-box')
+}).$mount('#app-box');
